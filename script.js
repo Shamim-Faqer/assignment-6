64e5417d0345e4c7888696b55d764fff20c471c0
Shamimm-Faqer <219502059+Shamim-Faqer@users.noreply.github.com>
2025-11-11 03:29:37 +0600

    navFunctionDone

--- script.js
+++ script.js
@@ -1,13 +1,43 @@
+// Navbar Heart Count Number
+
 const heartNumber = document.getElementById("navHeartCount");
 const neHeartButton = document.querySelectorAll(".neHeartBtn"); 
 
-neHeartButton.forEach(function(buttons) {buttons.addEventListener("click" , function() {
-  let life = Number(heartNumber.innerText);
-  life += 1;
-  heartNumber.innerText = life ;
-  
-} ) }
-  );
+neHeartButton.forEach(function(button) {
+  button.addEventListener("click", function() {
+    let life = Number(heartNumber.innerText);
+    life += 1;
+    heartNumber.innerText = life;
+  });
+});
 
 
-// alert("js connected😃")
+// Navbar Copy Count Number
+
+const navCopyNumber = document.getElementById("navCopyCountNumber");
+const cardCopyBtn = document.querySelectorAll(".gridCopyBtn"); 
+
+cardCopyBtn.forEach(function(btn) {
+  btn.addEventListener("click", function() {
+    let copyValues = Number(navCopyNumber.innerText);
+    copyValues += 1;
+    navCopyNumber.innerText = copyValues;
+  });
+}); 
+
+// Navbar Coin Count Number
+const navCoinCountNumber = document.getElementById("navCoinCount");
+const navCoinNumber = document.querySelectorAll(".gridCallBtn");
+
+navCoinNumber.forEach(function(callButtons) {callButtons.addEventListener("click",function() {
+  let totalCoins = Number( navCoinCountNumber.innerText
+    );
+    totalCoins -= 10;
+    navCoinCountNumber.innerText = totalCoins ;
+    
+} );
+} );
+// navCoinCount
+// gridCallBtn
+
+
