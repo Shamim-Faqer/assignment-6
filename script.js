8962b72d75e19b01bb067eea27e820aa1786fb21
Shamimm-Faqer <219502059+Shamim-Faqer@users.noreply.github.com>
2025-11-10 20:47:33 +0600

    responsive-complete

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-<<<<<<< HEAD
-console.log("Hello Bro");
-=======
-console.log("Hello Bro");
-alert("js connected succcesfully")
->>>>>>> 030b62e3672dc1d7e7879782fc2bfbcd3f0f1c56
+const heartNumber = document.getElementById("navHeartCount");
+const neHeartButton = document.querySelectorAll(".neHeartBtn"); 
+
+neHeartButton.forEach(function(buttons) {buttons.addEventListener("click" , function() {
+  let life = Number(heartNumber.innerText);
+  life += 1;
+  heartNumber.innerText = life ;
+  
+} ) }
+  );
+
+
+// alert("js connected😃")
